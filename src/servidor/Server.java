bb4f52ca9630a9ce312e084db8e1e0b43b359ab9
Caio <caiosant10@gmail.com>
2019-05-22 18:45:34 -0300

    Refactoring

--- src/servidor/Server.java
+++ src/servidor/Server.java
@@ -1,10 +1,7 @@
 package servidor;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
@@ -16,77 +13,58 @@ import java.util.TimeZone;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-    	/* cria um socket "servidor" associado a porta 8000
-        já aguardando conexões
-        */
-       ServerSocket servidor = new ServerSocket(8000);
-       //aceita a primeita conexao que vier
-       Socket socket = servidor.accept();
-       //verifica se esta conectado
-       if (socket.isConnected()) {
-           //imprime na tela o IP do cliente
-           System.out.println(socket.getInetAddress());
-           //cria um BufferedReader a partir do InputStream do cliente
-           BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-           System.out.println("Requisição: ");
-           /* Lê a primeira linha
-            contem as informaçoes da requisição
-            */
-           String linha = buffer.readLine();
-           //quebra a string pelo espaço em branco
-           String[] dadosReq = linha.split(" ");
-           //pega o metodo
-           String metodo = dadosReq[0];
-           //paga o caminho do arquivo
-           String caminhoArquivo = dadosReq[1];
-           //pega o protocolo
-           String protocolo = dadosReq[2];
-           //Enquanto a linha não for vazia
-           while (!linha.isEmpty()) {
-               //imprime a linha
-               System.out.println(linha);
-               //lê a proxima linha
-               linha = buffer.readLine();
-           }
-           //se o caminho foi igual a / entao deve pegar o /index.html
-           if (caminhoArquivo.equals("/")) {
-               caminhoArquivo = "src/servidor/index.html";
-           }
-           //abre o arquivo pelo caminho
-           File arquivo = new File(caminhoArquivo);
+        /* cria um socket "servidor" associado a porta 8000
+         já aguardando conexões
+         */
+        ServerSocket servidor = new ServerSocket(8000);
+        //aceita a primeita conexao que vier
+        Socket socket = servidor.accept();
+        //verifica se esta conectado
+        if (socket.isConnected()) {
+            //imprime na tela o IP do cliente
+            System.out.println(socket.getInetAddress());
+            //cria um BufferedReader a partir do InputStream do cliente
 
-           String status = protocolo + " 200 OK\r\n";
-           //se o arquivo não existe então abrimos o arquivo de erro, e mudamos o status para 404
-           if (!arquivo.exists()) {
-               status = protocolo + " 404 Not Found\r\n";
-               arquivo = new File("src/servidor/404.html"); 
-           }
+            RequestHTTP requisicao = RequestHTTP.lerRequisicao(socket.getInputStream());
 
-           //lê todo o conteúdo do arquivo para bytes
-           byte[] conteudo = Files.readAllBytes(arquivo.toPath());
-           //cria um formato para o GMT espeficicado pelo HTTP
-           SimpleDateFormat formatador = new SimpleDateFormat("E, dd MMM yyyy hh:mm:ss", Locale.ENGLISH);
-           formatador.setTimeZone(TimeZone.getTimeZone("GMT"));
-           Date data = new Date();
-           //Formata a dara para o padrao
-           String dataFormatada = formatador.format(data) + " GMT";
-           //cabeçalho padrão da resposta HTTP
-           String header = status
-                   + "Location: http://localhost:8000/\r\n"
-                   + "Date: " + dataFormatada + "\r\n"
-                   + "Server: MeuServidor/1.0\r\n"
-                   + "Content-Type: text/html\r\n"
-                   + "Content-Length: " + conteudo.length + "\r\n"
-                   + "Connection: close\r\n"
-                   + "\r\n";
-           //cria o canal de resposta utilizando o outputStream
-           OutputStream resposta = socket.getOutputStream();
-           //escreve o headers em bytes
-           resposta.write(header.getBytes());
-           //escreve o conteudo em bytes
-           resposta.write(conteudo);
-           //encerra a resposta
-           resposta.flush();
-       }
+            //se o caminho foi igual a / entao deve pegar o /index.html
+            if (requisicao.getRecurso().equals("/")) {
+                requisicao.setRecurso("src/servidor/index.html");
+            } else {
+            	requisicao.setRecurso("src/servidor/404.html");
+            }
+            //abre o arquivo pelo caminho
+            File arquivo = new File(requisicao.getRecurso());
+
+            ResponseHTTP resposta;
+
+            //se o arquivo existir então criamos a reposta de sucesso, com status 200
+            if (arquivo.exists()) {
+                resposta = new ResponseHTTP(requisicao.getProtocolo(), 200, "OK");
+            } else { 
+                //se o arquivo não existe então criamos a reposta de erro, com status 404
+                resposta = new ResponseHTTP(requisicao.getProtocolo(), 404, "Not Found");
+            }
+            //lê todo o conteúdo do arquivo para bytes e gera o conteudo de resposta
+            resposta.setConteudoResposta(Files.readAllBytes(arquivo.toPath()));
+            
+            //converte o formato para o GMT espeficicado pelo protocolo HTTP
+            SimpleDateFormat formatador = new SimpleDateFormat("E, dd MMM yyyy hh:mm:ss", Locale.ENGLISH);
+            formatador.setTimeZone(TimeZone.getTimeZone("GMT"));
+            Date data = new Date();
+            //Formata a data para o padrao
+            String dataFormatada = formatador.format(data) + " GMT";
+            
+            //cabeçalho padrão da resposta HTTP/1.1
+            resposta.setCabecalho("Location", "https://localhost:8000/");
+            resposta.setCabecalho("Date", dataFormatada);
+            resposta.setCabecalho("Server", "MeuServidor/1.0");
+            resposta.setCabecalho("Content-Type", "text/html");
+            resposta.setCabecalho("Content-Length",resposta.getTamanhoResposta());
+            //cria o canal de resposta utilizando o outputStream
+            resposta.setSaida(socket.getOutputStream());
+            resposta.enviar();
+
+        }
     }
 }
